f1b9b86720e767bd1026c2c2502bf67b346b2fe6
fadedreams7 <fadedreams7@gmail.com>
2023-11-15 08:57:06 -0500

    rabbitmq sevice+

---
+++ amqp1/go.mod
@@ -0,0 +1,5 @@
+module amqp1
+
+go 1.21.3
+
+require github.com/rabbitmq/amqp091-go v1.9.0 // indirect
---
+++ amqp1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	//connect to rabbitmq
+
+	//start listening to the queue
+
+	//create consuemr
+
+	//watch for messages
+}
